3fb7e541ccf58363bc8ca20b02f261d3211377db
AlexBaranosky <alexander.baranosky@gmail.com>
2011-05-06 20:54:15 -0400

    first unit test passes!

--- src/main.hs
+++ src/main.hs
@@ -1,6 +1,8 @@
 main = do
-   assertTrue
+   assertEquals 0 $ add ""
+
+add string =  0
+
+assertEquals expressionA expressionB = putStrLn $ if expressionA == expressionB then "yes!" else "you failed!"
 
 assertTrue expression = putStrLn $ if expression then "yes!" else "you failed!"
-
-assertEquals expressionA exptressionB = putStrLn $ if expressionA == expressionB then "yes!" else "you failed!"
