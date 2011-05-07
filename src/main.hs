e85971267da3acd31392a3d6946b4c668d6e1be5
AlexBaranosky <alexander.baranosky@gmail.com>
2011-05-06 21:19:38 -0400

    can now add '2,3'

--- src/main.hs
+++ src/main.hs
@@ -1,11 +1,17 @@
+import Data.List.Split
+
 main = do
    assertEquals "blank string adds up to 0" 0 $ add ""
-   assertEquals "one number alone adds up to itself" 1 $ add "1"
+   assertEquals "one adds up to one" 1 $ add "1"
    assertEquals "one number alone adds up to itself" 2 $ add "2"
+   assertEquals "adds two comma-separated numbers" 5 $ add "2,3"
 
 add string = case string of
    "" -> 0
-   otherwise -> read string ::Int
+   otherwise -> sum $ numbersFrom string
+
+numbersFrom :: String -> [Int]
+numbersFrom = map read . splitOn ","
 
 assertEquals msg expressionA expressionB = putStrLn $ if expressionA == expressionB then "yes!" else "you failed: " ++ msg
 
