21f91863f04c0e2f446ec1a98376f3e90e1ac5ab
AlexBaranosky <alexander.baranosky@gmail.com>
2011-05-06 21:04:52 -0400

    now adds a solo number correctly, if not always '1' :)

--- src/main.hs
+++ src/main.hs
@@ -1,10 +1,11 @@
 main = do
    assertEquals "blank string adds up to 0" 0 $ add ""
    assertEquals "one number alone adds up to itself" 1 $ add "1"
+   assertEquals "one number alone adds up to itself" 2 $ add "2"
 
-add string =  case string of
+add string = case string of
    "" -> 0
-   otherwise -> 1
+   otherwise -> read string ::Int
 
 assertEquals msg expressionA expressionB = putStrLn $ if expressionA == expressionB then "yes!" else "you failed: " ++ msg
 
