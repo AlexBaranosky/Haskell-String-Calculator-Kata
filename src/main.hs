ee7059903142612e4df8e0fdc930991d117f52c4
AlexBaranosky <alexander.baranosky@gmail.com>
2011-05-06 21:02:08 -0400

    now adds a solo number correctly

--- src/main.hs
+++ src/main.hs
@@ -1,8 +1,11 @@
 main = do
-   assertEquals 0 $ add ""
+   assertEquals "blank string adds up to 0" 0 $ add ""
+   assertEquals "one number alone adds up to itself" 1 $ add "1"
 
-add string =  0
+add string =  case string of
+   "" -> 0
+   otherwise -> 1
 
-assertEquals expressionA expressionB = putStrLn $ if expressionA == expressionB then "yes!" else "you failed!"
+assertEquals msg expressionA expressionB = putStrLn $ if expressionA == expressionB then "yes!" else "you failed: " ++ msg
 
 assertTrue expression = putStrLn $ if expression then "yes!" else "you failed!"
